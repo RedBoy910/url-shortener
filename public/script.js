13b217ab3818106d43742f708a47f92b4a55db3e
Tudor Pasca <tudorpasca11@gmail.com>
2021-10-16 18:47:12 +0300

    changed domain

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 function submit() {
     const input = document.getElementById("url-input").value;
-    const url = "https://powerful-mountain-78340.herokuapp.com/api/urls";
+    const url = "http://www.tudorlink.com/api/urls";
     const data = {
         url: input
     };
